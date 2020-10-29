64ba6abcf9b4662c70d9805773643ef1909b9695
LukaszI <lukasz.inglot@leocode.com>
2020-10-29 04:13:14 +0100

    Changed styles in UserList styled.ts

--- src/components/UserList/styled.ts
+++ src/components/UserList/styled.ts
@@ -3,7 +3,14 @@ import styled from 'styled-components';
 export const StyledUserList = styled.ul`
   display: flex;
   flex-direction: column;
-  
-  margin: 1.5rem 0 0 11rem;
-  width: 500px;
+  align-content:center;
+  justify-content: center;
+  margin: 1.5rem auto;
+  width: 300px;
+  padding:0;
+`;
+
+export const StyledSpan = styled.span`
+  background-color: red;
+  font-size: 20px;
 `;
